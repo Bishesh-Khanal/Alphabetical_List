bbfd10a1f9f851d5cfbbf2ecf6203f9a4b8dd46d
Bishesh Khanal <bishesh.khanal25@gmail.com>
2022-10-16 20:05:49 -0700

    Created names.cpp
    Take names to be arranged albetically

---
+++ names.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+void setNames( string names[], int size ){
+    cout << "Enter the names in random order:" << endl;
+    for( int i = 0; i < size; i++ ){
+        getline( cin, names[i] );
+        getchar();
+    }
+}
