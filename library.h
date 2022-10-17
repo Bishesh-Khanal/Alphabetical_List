98ed0a197da82893f218349840730762832f1767
Bishesh Khanal <bishesh.khanal25@gmail.com>
2022-10-16 20:47:24 -0700

    Created library.h
    Contains list of functions used in the project

---
+++ library.h
@@ -0,0 +1,7 @@
+using std::string;
+
+void setSize( int& );
+void setNames( string[], int );
+void sortNames( string[], int, int, int );
+void Swap( string&, string& );
+void displayNames( string[], int );
